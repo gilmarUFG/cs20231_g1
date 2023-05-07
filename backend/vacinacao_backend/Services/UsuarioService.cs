63c7c205d35bfc0dd987b41e2105924310d1fc02
joaopedrosf <joaopedrosf1903@gmail.com>
2023-05-07 12:30:56 -0300

    CRUD Inicial API
    - Criação das entidades
    - Criação dos services
    - Criação do repository
    - Criação dos controllers
    - Criação das migrations do banco de dados
    - Implementação dos testes automatizados para os services

---
+++ backend/vacinacao_backend/Services/UsuarioService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vacinacao_backend.Models;
+using vacinacao_backend.Repositories;
+
+namespace vacinacao_backend.Services {
+    public class UsuarioService {
+
+        private readonly VacinacaoContext _vacinacaoContext;
+
+        public UsuarioService(VacinacaoContext vacinacaoContext) {
+            _vacinacaoContext = vacinacaoContext;
+        }
+
+        public async Task<List<Usuario>> FindAllUsuarios() {
+            return await _vacinacaoContext.Usuarios.AsNoTracking().Include(u => u.Alergias).AsNoTracking().Include(u => u.Agendamentos).ThenInclude(a => a.Vacina).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Usuario> FindUsuarioById(int id) {
+            var usuarioExists = _vacinacaoContext.Usuarios.Any(u => u.Id == id);
+            if (!usuarioExists) {
+                throw new ArgumentException("Usuário não encontrado");
+            }
+            var usuario = await _vacinacaoContext.Usuarios.AsNoTracking().Where(u => u.Id == id).Include(u => u.Alergias).AsNoTracking().Include(u => u.Agendamentos).ThenInclude(a => a.Vacina).AsNoTracking().FirstAsync();
+
+            return usuario;
+        }
+
+        public async Task InsertUsuario(InsertUsuarioDTO usuarioDto) {
+            var usuario = new Usuario(usuarioDto);
+            usuario.Alergias = await _vacinacaoContext.Alergias.AsNoTracking().Where(a => usuarioDto.Alergias.Contains(a.Id)).ToListAsync();
+            await _vacinacaoContext.Usuarios.AddAsync(usuario);
+            await _vacinacaoContext.SaveChangesAsync();
+            _vacinacaoContext.Usuarios.Entry(usuario).State = EntityState.Detached;
+        }
+
+        public async Task DeleteUsuario(int id) {
+            var usuario = new Usuario { Id = id };
+            _vacinacaoContext.Usuarios.Attach(usuario);
+            _vacinacaoContext.Usuarios.Remove(usuario);
+            await _vacinacaoContext.SaveChangesAsync();
+        }
+    }
+}
