27c68f4c75fafef4769670b000e4dd2d4a3ab4a3
igor-pimenta-araujo <igor.araujo0147@gmail.com>
2023-07-17 19:40:51 -0300

    Atualização Nuxt 2 para Nuxt 3
    Depois de avaliar os benefícios de atualizar para o Nuxt 3, decidimos que era a melhor decisão para o nosso projeto. Acreditamos que os novos recursos e melhorias do Nuxt 3 nos ajudarão a criar um aplicativo mais rápido, escalável e fácil de manter. A atualização foi concluída com sucesso e não esperamos nenhum impacto negativo no desempenho do nosso aplicativo. Estamos confiantes de que a atualização para o Nuxt 3 será uma melhoria geral para nosso projeto.

--- vacina/components/VacinaRoot.vue
+++
@@ -1,9 +0,0 @@
-
-<template>
-  <div>
-    <menu-sidebar/>
-    <div class="p-4 sm:ml-64">
-      <indexs-vacina />
-    </div>
-  </div>
-</template>
--- vacina/pages/index.vue
+++ vacina/pages/index.vue
@@ -1,25 +1,9 @@
-<template>
-  <div>
-    <login
-      :admin=false
-    />
-  </div>
-</template>
-
 <script setup>
-import Vue, { onMounted, onUnmounted } from 'vue';
 
-function verifyToken() {
-  const token = localStorage.getItem('token');
-  if (token) {
-    return true;
-  }
-  return false;
-}
-
-onMounted(() => {
-  if (verifyToken()) {
-    window.location.href = '/agendamento';
-  }
-});
 </script>
+
+<template>
+    <div>
+        <cookie />
+    </div>
+</template>
